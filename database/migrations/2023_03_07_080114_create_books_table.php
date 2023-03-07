90c0d2d161d3e3a0a2e6e3e50705de741e61c5ba
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-03-07 10:45:31 +0200

    02. إنشاء قاعدة البيانات

---
+++ database/migrations/2023_03_07_080114_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id')->nullable(); // could be empty
+            $table->unsignedBigInteger('publisher_id')->nullable();
+            $table->string('title');
+            $table->string('isbn')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('publish_year')->nullable();
+            $table->unsignedBigInteger('number_of_pages');
+            $table->unsignedBigInteger('number_of_copies');
+            $table->decimal('price', 8, 2); // 8 max digits 2 digits after ,00
+            $table->string('cover_images'); // image path
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null'); // on deleting the category the book category_id will be set to null
+            $table->foreign('publisher_id')->references('id')->on('publishers')->onDelete('set null'); // on deleting the publisher the book publisher_id will be set to null
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2023_03_07_083213_create_book_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('number_of_copies')->default(1); // number of items in cart
+            $table->boolean('bought')->default(false);
+            $table->decimal('price', 8, 2)->default(0); // price here is deferent from price on books table this is the price of the book when the user bought it will not change if the actual price of the book changed
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book_user');
+    }
+};
