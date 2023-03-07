1f016f5fea1d3ca9274107367614d8e4148e654d
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-03-07 11:21:31 +0200

    03. ربط النماذج بالعلاقات

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    public function books() // the author can have many books and the book can by owned my many authors
+    {
+        return $this->belongsToMany(Book::class , 'book_author'); // many to many , by default Laravel assumes the table is author_book
+    }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function publisher()
+    {
+        return $this->belongsTo(Publisher::class);
+    }
+
+    public function authors() // the book can by owned my many authors and the author can have many books and
+    {
+        return $this->belongsToMany(Author::class , 'book_author'); // many to many , by default Laravel assumes the table is author_book
+    }
 }
