51af55b726d8597aa955f9771cca20df4c9399ab
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-03-08 14:01:40 +0200

    06. توجيه ومتحكم الصفحة الرئيسية

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GalleryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('layouts.main');
     })->name('dashboard');
 });
+
+Route::get('/', [GalleryController::class, 'index'])->name('gallery.index');
