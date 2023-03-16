c606f1cc1b5fcbdc02c2889a43171b9c65f7af31
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-03-16 07:40:59 +0200

    03. إنشاء حساب paypal developer

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -64,8 +64,9 @@
 
                             <h4 class="mb-5">{{ __('السعر النهائي:') }} {{ $totalPrice }} $</h4>
 
-                            <!-- Set up a container element for the button -->
+                            <!-- Set up a container element for paypal button -->
                             <div class="d-inline-block" id="paypal-button-container"></div>
+
                             <a href="" class="d-inline-block mb-4 float-start btn bg-cart"
                                 style="text-decoration:none;">
                                 <span> {{ __('بطاقة ائتمانية') }}</span>
@@ -84,4 +85,51 @@
 @endsection
 
 @section('script')
+    <!-- Replace "test" with your own sandbox Business account app client ID -->
+    <script
+        src="https://www.paypal.com/sdk/js?client-id=Ad9KWQWeBL6Zqg45C56qx-oc00j6qo-MR-Wx7DCwui8EzGTWOe2U6MjpP9GNZv4c7G6PpL6MAGHvn2cy&currency=USD">
+    </script>
+
+    <script>
+        paypal.Buttons({
+            // Order is created on the server and the order id is returned
+            createOrder() {
+                return fetch("/my-server/create-paypal-order", {
+                        method: "post",
+                        // use the "body" param to optionally pass additional order information
+                        // like product skus and quantities
+                        body: JSON.stringify({
+                            cart: [{
+                                sku: "YOUR_PRODUCT_STOCK_KEEPING_UNIT",
+                                quantity: "YOUR_PRODUCT_QUANTITY",
+                            }, ],
+                        }),
+                    })
+                    .then((response) => response.json())
+                    .then((order) => order.id);
+            },
+            // Finalize the transaction on the server after payer approval
+            onApprove(data) {
+                return fetch("/my-server/capture-paypal-order", {
+                        method: "post",
+                        body: JSON.stringify({
+                            orderID: data.orderID
+                        })
+                    })
+                    .then((response) => response.json())
+                    .then((orderData) => {
+                        // Successful capture! For dev/demo purposes:
+                        console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                        const transaction = orderData.purchase_units[0].payments.captures[0];
+                        alert(
+                            `Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`
+                        );
+                        // When ready to go live, remove the alert and show a success message within this page. For example:
+                        // const element = document.getElementById('paypal-button-container');
+                        // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+                        // Or go to another URL:  window.location.href = 'thank_you.html';
+                    });
+            }
+        }).render('#paypal-button-container');
+    </script>
 @endsection
